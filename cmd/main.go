d60db2298b7b10335ee149ecd8203bcdcb100cb2
elvesbd <elvesbd@yahoo.com>
2022-04-01 19:49:12 -0300

    add pkg

---
+++ cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"hex/internal/adapters/core/arithmetic"
+	"hex/internal/ports"
+)
+
+func main() {
+	// ports
+	var core ports.ArithmeticPort
+	core = arithmetic.NewAdapter()
+
+	/* arithAdapter := arithmetic.NewAdapter()
+	result, err := arithAdapter.Addition(1, 3)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(result) */
+}
