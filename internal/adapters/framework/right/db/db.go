ac4eb88a30045ae1ad3d547943770e38eb36aaee
elvesbd <elvesbd@yahoo.com>
2022-04-01 19:48:29 -0300

    create db connection

---
+++ internal/adapters/framework/right/db/db.go
@@ -0,0 +1,51 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"time"
+
+	sq "github.com/Masterminds/squirrel"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Adapter struct {
+	db *sql.DB
+}
+
+func NewAdapter(driverName, dataSourceName string) (*Adapter, error) {
+	// connection to database
+	db, err := sql.Open(driverName, dataSourceName)
+	if err != nil {
+		log.Print("db connection failure: %w", err)
+	}
+	// test db connection
+	err = db.Ping()
+	if err != nil {
+		log.Print("db ping failure: %w", err)
+	}
+
+	return &Adapter{db: db}, nil
+}
+
+func (da Adapter) CloseDbConnection() {
+	err := da.db.Close()
+	if err != nil {
+		log.Print("db close failure: %w", err)
+	}
+}
+
+func (da Adapter) AddToHistory(answer int32, operation string) error {
+	queryString, args, err := sq.Insert("airth_history").Columns("date", "answer", "operation").
+		Values(time.Now(), answer, operation).ToSql()
+	if err != nil {
+		return err
+	}
+
+	_, err = da.db.Exec(queryString, args...)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
