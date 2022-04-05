b880551d4445f39743c3e798a9a53731d5209f7f
elvesbd <elvesbd@yahoo.com>
2022-04-05 19:17:35 -0300

    updated

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,40 @@
 package main
 
 import (
+	// application
+	"hex/internal/adapters/app/api"
 	"hex/internal/adapters/core/arithmetic"
+
 	"hex/internal/ports"
+	"log"
+	"os"
+
+	// adapters
+	gRPC "hex/internal/adapters/framework/left/grpc"
+	"hex/internal/adapters/framework/right/db"
 )
 
 func main() {
+	var err error
+
 	// ports
+	var dbAdapter ports.DbPort
 	var core ports.ArithmeticPort
-	core = arithmetic.NewAdapter()
+	var appAdapter ports.APIPort
+	var gRPCAdapter ports.GRPCPort
 
-	/* arithAdapter := arithmetic.NewAdapter()
-	result, err := arithAdapter.Addition(1, 3)
+	dbDriver := os.Getenv("DB_DRIVER")
+	dbSourceName := os.Getenv("DB_NAME")
 
+	dbAdapter, err = db.NewAdapter(dbDriver, dbSourceName)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalf("failed to initiate db connection: %v", err)
 	}
+	defer dbAdapter.CloseDbConnection()
 
-	fmt.Println(result) */
+	core = arithmetic.NewAdapter()
+	appAdapter = api.NewAdapter(dbAdapter, core)
+
+	gRPCAdapter = gRPC.NewAdapter(appAdapter)
+	gRPCAdapter.Run()
 }
